e8e201d24b36b3df8872adce8d9946035798b6a1
Uwe Schmidt <mail@uweschmidt.org>
2021-07-22 21:50:52 +0200

    Track outbound links

--- _static/js/plausible.js
+++ _static/js/plausible.js
@@ -1,1 +1,1 @@
-document.write('<script defer data-domain="stardist.net" src="https://plausible.io/js/plausible.js"></script>')
+document.write('<script defer data-domain="stardist.net" src="https://plausible.io/js/plausible.outbound-links.js"></script>')
